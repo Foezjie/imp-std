1e2541c0a4d70648db46e40dda847b5565eefdf1
Harm De Weirdt <harm.deweirdt@gmail.com>
2013-11-22 11:37:19 +0100

    Added: * package before service * file before service (bruteforced: all files must be made before the services)

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -44,24 +44,53 @@ def dir_before_file(model, resources):
             for dir in host.directories:
                 dir_res = Resource.get_resource(dir)
                 if dir_res is not None and os.path.dirname(resource.path) == dir_res.path:
+                    #Make the File resource require the directory
                     resource.requires.add(dir_res.id)
 
 @dependency_manager
 def package_before_service(model, resources):
     """
-        If a file is defined on a host, then make the file depend on its parent directory
+        If a service is defined on a host, then make the service depend on all packages
+        A brute force way of defining pkg->srv dependencies.
+        Optional better way:
+            1. somehow ask the package manager what service a package installs
+            2. see if that service is described in the model
+            3. if so: get the resource of that service and make it depend on the original package
     """
-    # loop over all resources to find files
+    # loop over all resources to find services
     for _id, resource in resources.items():
         res_class = resource.model.__class__
-        if resource.model.__module__ == "std" and res_class.__name__ == "File":
+        if resource.model.__module__ == "std" and res_class.__name__ == "Service":
             model = resource.model
             host = model.host
 
-            # now find all packages on the same host as the yum repo file and add the repo as a
-            # dependency if it is not already a dependency
-            for dir in host.directories:
-                dir_res = Resource.get_resource(dir)
-                if dir_res is not None :
-                    if _id not in dir_res.requires and os.path.dirname(resource.path) == dir_res.path:
-                        dir_res.requires.add(_id)
+            # now find all packages on the same host as the service and add the packages as a requirement
+            for pkg in host.services:
+                pkg_res = Resource.get_resource(pkg)
+                if pkg_res is not None:
+                        #Make the Service resource require the package
+                        resource.requires.add(pkg_res.id)
+
+@dependency_manager
+def file_before_service(model, resources):
+    """
+        If a service is defined on a host, then make the service depend on all packages
+        A brute force way of defining pkg->srv dependencies.
+        Optional better way:
+            1. somehow ask the package manager what service a package installs
+            2. see if that service is described in the model
+            3. if so: get the resource of that service and make it depend on the original package
+    """
+    # loop over all resources to find services
+    for _id, resource in resources.items():
+        res_class = resource.model.__class__
+        if resource.model.__module__ == "std" and res_class.__name__ == "Service":
+            model = resource.model
+            host = model.host
+
+            # now find all packages on the same host as the service and add the packages as a requirement
+            for file in host.files:
+                file_res = Resource.get_resource(file)
+                if file_res is not None:
+                        #Make the Service resource require the package
+                        resource.requires.add(file_res.id)
