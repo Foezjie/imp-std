bb83070e1b3642f29212e58f82d106bdc3834f3a
Harm De Weirdt <harm.deweirdt@gmail.com>
2013-11-26 14:59:17 +0100

    Worked out the idea of scope/service stack dependencies Doesn't work yet. id-attribute not found.

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -29,68 +29,59 @@ def unique_file(prefix : "string", seed : "string", suffix : "string") -> "strin
     return prefix + hashlib.md5(seed.encode("utf-8")).hexdigest() + suffix
 
 @dependency_manager
-def dir_before_file(model, resources):
+def scope_dependencies(model, resources):
     """
-        If a file is defined on a host, then make the file depend on its parent directory
+        *Heuristic*
+        File, package and service entities defined in the same scope almost always define a "stack" representing a service
+          consisting of its configfile, the package that installs it and the service itself.
+        We use this information to automatically create the required dependencies between them.
+        [Package --> (Config)File --> Service]
     """
-    # loop over all resources to find files
-    for _id, resource in resources.items():
-        res_class = resource.model.__class__
-        if resource.model.__module__ == "std" and res_class.__name__ == "File":
-            model = resource.model
-            host = model.host
+    #first we look for all "service stacks": scopes that have a service, a (config) file and a package
+    srv_stacks = find_srv_stacks(resources)
+    for stack in srv_stacks:
+        setup_stack_deps(stack)
 
+def find_srv_stacks(resources):
+    #list of 3-tuples of resources that are in the same scope. (files,services, packages)
+    stacks = []
+    for res in resources.values():
+        scope_pkg = []
+        scope_cfg = []
+        scope_srv = []
+        model_instance = res.model
+        instance_scope = model_instance.__scope__
+        scope_vars = instance_scope.variables()
+        scope_cfg = filter((lambda x: x.value.__class__.__name__ == "File"), scope_vars)
+        scope_srv = filter((lambda x: x.value.__class__.__name__ == "Service"), scope_vars)
+        scope_pkg = filter((lambda x: x.value.__class__.__name__ == "Package"), scope_vars)
+        #Now get the actual value in the variable
+        #scope_cfg = map((lambda x: x.value), scope_cfg)
+        #scope_srv = map((lambda x: x.value), scope_cfg) Error want in python3 is het resultaat van filter geen list meer
+        #scope_pkg = map((lambda x: x.value), scope_cfg)
+        if scope_pkg and scope_srv and scope_cfg:
+            stacks.append({'pkg': scope_pkg, 'srv': scope_srv, 'cfg': scope_cfg})
 
-            for dir in host.directories:
-                dir_res = Resource.get_resource(dir)
-                if dir_res is not None and os.path.dirname(resource.path) == dir_res.path:
-                    #Make the File resource require the directory
-                    resource.requires.add(dir_res.id)
+        return stacks
 
-@dependency_manager
-def package_before_service(model, resources):
-    """
-        If a service is defined on a host, then make the service depend on all packages
-        A brute force way of defining pkg->srv dependencies.
-        Optional better way:
-            1. somehow ask the package manager what service a package installs
-            2. see if that service is described in the model
-            3. if so: get the resource of that service and make it depend on the original package
-    """
-    # loop over all resources to find services
-    for _id, resource in resources.items():
-        res_class = resource.model.__class__
-        if resource.model.__module__ == "std" and res_class.__name__ == "Service":
-            model = resource.model
-            host = model.host
-
-            # now find all packages on the same host as the service and add the packages as a requirement
-            for pkg in host.services:
-                pkg_res = Resource.get_resource(pkg)
-                if pkg_res is not None:
-                        #Make the Service resource require the package
-                        resource.requires.add(pkg_res.id)
-
-@dependency_manager
-def file_before_service(model, resources):
-    """
-        If a service is defined on a host, then make the service depend on all packages
-        A brute force way of defining pkg->srv dependencies.
-        Optional better way:
-            1. somehow ask the package manager what service a package installs
-            2. see if that service is described in the model
-            3. if so: get the resource of that service and make it depend on the original package
-    """
-    # loop over all resources to find services
-    for _id, resource in resources.items():
-        res_class = resource.model.__class__
-        if resource.model.__module__ == "std" and res_class.__name__ == "Service":
-            model = resource.model
-            host = model.host
-
-            # now find all packages on the same host as the service and add the packages as a requirement
-            for file in host.files:
-                file_res = Resource.get_resource(file)
-                if file_res is not None:
-                        #Make the Service resource require the package
-                        resource.requires.add(file_res.id)
+def setup_stack_deps(stack):
+    for srv in stack['srv']:
+        srv = srv.value
+        for pkg in stack['pkg']:
+            pkg = pkg.value
+            print(pkg)
+            if pkg.id not in srv.requires:
+                srv.requires.add(pkg.id)
+                print(pkg.id + " toegevoegd aan " + srv)
+        for cfg in stack['cfg']:
+            cfg = cfg.value
+            if cfg.id not in srv.requires:
+                srv.requires.add(cfg.id)
+                print(cfg.id + " toegevoegd aan " + srv)
+    for cfg in stack[cfg]:
+        cfg = cfg.value
+        for pkg in stack['pkg']:
+            pkg = pkg.value
+            if pkg.id not in cfg.requires:
+                cfg.requires.add(pkg.id)
+                print(kg.id + " toegevoegd aan " + cfg)
