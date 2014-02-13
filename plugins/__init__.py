3034b1887025202540d76c56c17a07d84e04c930
Harm De Weirdt <harm.deweirdt@gmail.com>
2014-02-13 14:22:50 +0100

    Added "service stack" heuristic

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -94,3 +94,62 @@ def file_before_service(model, resources):
                 if file_res is not None:
                         #Make the Service resource require the package
                         resource.requires.add(file_res.id)
+
+@dependency_manager
+def scope_dependencies(model, resources):
+    """
+        *Heuristic*
+        File, package and service entities defined in the same scope almost always define a "stack" representing a service
+          consisting of its configfile, the package that installs it and the service itself.
+        We use this information to automatically create the required dependencies between them.
+        [Package --> (Config)File --> Service]
+    """
+    #first we look for all "service stacks": scopes that have a service, a (config) file and a package
+    srv_stacks = find_srv_stacks(resources)
+    for stack in srv_stacks:
+        setup_stack_deps(stack)
+        print("Stack toegevoegd")
+
+def find_srv_stacks(resources):
+    #list of 3-tuples of resources that are in the same scope. (files,services, packages)
+    stacks = []
+    for res in resources.values():
+        scope_pkg = []
+        scope_cfg = []
+        scope_srv = []
+        model_instance = res.model
+        instance_scope = model_instance.__scope__
+        scope_vars = instance_scope.variables()
+        scope_cfg = filter((lambda x: x.value.__class__.__name__ == "File"), scope_vars)
+        scope_srv = filter((lambda x: x.value.__class__.__name__ == "Service"), scope_vars)
+        scope_pkg = filter((lambda x: x.value.__class__.__name__ == "Package"), scope_vars)
+        #Now get the actual value in the variable
+        #scope_cfg = map((lambda x: x.value), scope_cfg)
+        #scope_srv = map((lambda x: x.value), scope_cfg) Error want in python3 is het resultaat van filter geen list meer
+        #scope_pkg = map((lambda x: x.value), scope_cfg)
+        if scope_pkg and scope_srv and scope_cfg:
+            stacks.append({'pkg': scope_pkg, 'srv': scope_srv, 'cfg': scope_cfg})
+
+        return stacks
+
+def setup_stack_deps(stack):
+    for srv in stack['srv']:
+        srv = Resource.get_resource(srv.value)
+        for pkg in stack['pkg']:
+            pkg = Resource.get_resource(pkg.value)
+            print(pkg)
+            if pkg.id not in srv.requires:
+                srv.requires.add(pkg.id)
+                print(pkg.id + " toegevoegd aan " + srv)
+        for cfg in stack['cfg']:
+            cfg = Resource.get_resource(cfg.value)
+            if cfg.id not in srv.requires:
+                srv.requires.add(cfg.id)
+                print(cfg.id + " toegevoegd aan " + srv)
+    for cfg in stack[cfg]:
+        cfg = Resource.get_resource(cfg.value)
+        for pkg in stack['pkg']:
+            pkg = Resource.get_resource(pkg.value)
+            if pkg.id not in cfg.requires:
+                cfg.requires.add(pkg.id)
+                print(pkg.id + " toegevoegd aan " + cfg)
